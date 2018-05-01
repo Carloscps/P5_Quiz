b581c41096972c8d49564b9f8a1faa5fd8be51c6
jorgesr97 <jsrevuelta@telefonica.net>
2018-05-01 22:11:20 +0200

    V1

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const quizController = require('../controllers/quiz');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quiz' });
+});
+
+//Credits page.
+router.get('/credits', function(req, res, next) {
+    res.render('credits');
+});
+
+//List of quizzes
+router.get('/quizzes', quizController.index);
+module.exports = router;
